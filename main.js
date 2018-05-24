37e492b9cda0a96d31e6ca676b84784488aeafd4
wambatch <39588962+wambatch@users.noreply.github.com>
2018-05-24 18:26:36 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+// Your user has no adblocker, good for them
+if (window.showAds){document.getElementById('RDqbiLxKnUQF').style.display='none';}
+// You user uses adbocker, do something here
+else{document.getElementById('RDqbiLxKnUQF').style.display='block';}
