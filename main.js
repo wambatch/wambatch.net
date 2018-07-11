a94808a9cb75370b6f8786f32dae90780577eee6
wambatch <39588962+wambatch@users.noreply.github.com>
2018-07-12 01:09:41 +0700

    Delete main.js

--- main.js
+++
@@ -1,4 +0,0 @@
-// Your user has no adblocker, good for them
-if (window.showAds){document.getElementById('RDqbiLxKnUQF').style.display='none';}
-// You user uses adbocker, do something here
-else{document.getElementById('RDqbiLxKnUQF').style.display='block';}
